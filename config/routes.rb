78c6b154dab7f0aa2009c1231fff65290f1bb6aa
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2020-04-11 10:01:48 +0800

    Basic Authentication using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  path: '',
+  path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'}
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
