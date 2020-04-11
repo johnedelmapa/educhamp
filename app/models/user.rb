1d965e8d3ea4b6c8a2f48b5a110e39d003af5e18
Johnedel S. Mapa <34313943+johnedelmapa@users.noreply.github.com>
2020-04-11 14:40:50 +0800

    Finish Edit Profile (#3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :omniauthable
   validates :fullname, presence: true
+  mount_uploader :image, ImageUploader
 
   def self.from_omniauth(auth)
     user = User.where(email: auth.info.email).first
