3c8a78be4be831441864b700c3867d2579d1a44e
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2020-04-11 14:40:07 +0800

    Finish Edit Profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def avatar_url(user)
+    @image = user.image
+    if user.uid
+      "https://graph.facebook.com/#{user.uid}/picture?type=large"
+    else
+      if user.image.present?
+        @image.url
+      else
+        "default.png"
+      end
+    end
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
   path: '',
   path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'},
-  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+  controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
