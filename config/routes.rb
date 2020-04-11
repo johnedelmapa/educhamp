5dbb610c1f70a148211e2900e82070db61640694
Johnedel S. Mapa <34313943+johnedelmapa@users.noreply.github.com>
2020-04-11 16:06:48 +0800

    Inital Showpage (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'},
   controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   root 'pages#home'
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
