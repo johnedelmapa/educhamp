daf499f192f31b22b16163a3a81f2924c32cb596
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2020-04-11 10:38:49 +0800

    Facebook Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
   path: '',
-  path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'}
+  path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'},
+  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
